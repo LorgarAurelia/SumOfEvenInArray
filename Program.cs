eb2ecdc4815a884ee7073350dfec5976d115966f
David Maltsev <luterfaeron@gmail.com>
2021-07-20 19:56:30 +0300

    Суммирование чисел в массиве

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace SumOfEvenInArray
 {
     class Program
     {
         static void Main(string[] args)
         {
+            uint[] arrayOfNumbers =  new uint[4] {1, 2, 3, 4};
+
+            uint sum = 0;
+
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+                sum = (arrayOfNumbers[i] % 2 == 0) ? sum + arrayOfNumbers[i] : sum;
+            }
+
+            Console.WriteLine("Сумма четных чисел в массиве равна " + sum);
+            _ = Console.ReadLine();
         }
     }
 }
